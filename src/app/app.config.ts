143134c7a993ce4969f13d4713b37ec3cd11ea56
Carlos M. Sistiaga <sistiaga@gmail.com>
2026-02-08 15:25:54 +0100

    Estable 8/2/26

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,12 +5,29 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { TranslateModule } from '@ngx-translate/core';
 import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+// master - MARSISCA - BEGIN 2026-02-07
+import { provideNativeDateAdapter, MAT_DATE_FORMATS, DateAdapter, MAT_DATE_LOCALE } from '@angular/material/core';
+// master - MARSISCA - END 2026-02-07
 
 import { routes } from './app.routes';
 // master - MARSISCA - BEGIN 2026-02-06
 import { provideApiConfiguration } from './api/api-configuration';
 import { environment } from '../environments/environment';
 // master - MARSISCA - END 2026-02-06
+
+// master - MARSISCA - BEGIN 2026-02-07
+export const DD_MM_YYYY_FORMAT = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: { day: '2-digit', month: '2-digit', year: 'numeric' } as Intl.DateTimeFormatOptions,
+    monthYearLabel: { year: 'numeric', month: 'short' } as Intl.DateTimeFormatOptions,
+    dateA11yLabel: { year: 'numeric', month: 'long', day: 'numeric' } as Intl.DateTimeFormatOptions,
+    monthYearA11yLabel: { year: 'numeric', month: 'long' } as Intl.DateTimeFormatOptions,
+  },
+};
+// master - MARSISCA - END 2026-02-07
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -24,8 +41,13 @@ export const appConfig: ApplicationConfig = {
     ),
     provideTranslateHttpLoader(),
     // master - MARSISCA - BEGIN 2026-02-06
-    provideApiConfiguration(environment.apiUrl)
+    provideApiConfiguration(environment.apiUrl),
     // master - MARSISCA - END 2026-02-06
+    // master - MARSISCA - BEGIN 2026-02-07
+    provideNativeDateAdapter(),
+    { provide: MAT_DATE_FORMATS, useValue: DD_MM_YYYY_FORMAT },
+    { provide: MAT_DATE_LOCALE, useValue: 'es-ES' }
+    // master - MARSISCA - END 2026-02-07
   ]
 };
 // master - MARSISCA - END 2025-12-08
